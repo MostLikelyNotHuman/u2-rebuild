7100bf5b0c048d4bbe082bc25d4a5d14d48480eb
MostLikelyNotHuman <55810556+MostLikelyNotHuman@users.noreply.github.com>
2026-03-21 05:14:10 -0500

    header layout and appearance, mobile menu added

---
+++ mfbr2/src/components/basic/HeaderRoutes.jsx
@@ -0,0 +1,22 @@
+export const HeaderRoutes = [
+    {
+        name: "Notes",
+        link: "/notes-practice"
+    },
+    {
+        name: "Intervals",
+        link: "/intervals-practice"
+    },
+    {
+        name: "Help",
+        link: "/help"
+    },
+    {
+        name: "Contact",
+        link: "/contact"
+    },
+    {
+        name: "Log In",
+        link: "/login"
+    }
+];
