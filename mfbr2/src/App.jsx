5d37384271f6beb965c0ceb228458521b947b085
MostLikelyNotHuman <55810556+MostLikelyNotHuman@users.noreply.github.com>
2026-04-26 23:48:53 -0500

    homepage route, ready to build navigation box in body

--- mfbr2/src/App.jsx
+++ mfbr2/src/App.jsx
@@ -1,12 +1,17 @@
 import './App.css'
 import Header from './components/basic/Header';
 import Footer from './components/basic/Footer';
+import { Route, Routes } from 'react-router';
+import HomePage from './components/HomePage';
 
 function App() {
   
   return (
     <>
       <Header></Header>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
       <Footer></Footer>
     </>  
   );
---
+++ mfbr2/src/components/HomePage.jsx
@@ -0,0 +1,24 @@
+
+
+
+
+
+const HomePage = () => {
+    return (
+        <main className="homePage-main">
+            <div id="homePage-intro">
+                <h2>Welcome to Music for Beginners!</h2>
+                <p>Music For Beginners is an app designed to teach you the very basics 
+                    of reading music. Here you'll find exercises to strengthen your ability 
+                    to identify notes and intervals on the staff, with the goal of creating a solid 
+                    foundation of skills for you to build on in the future!
+                </p>
+            </div>
+            <div id="homePage-startBox">
+                {/* box */}
+            </div>
+        </main>
+    );
+}
+
+export default HomePage;
